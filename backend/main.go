e9860df80fac982e0d52405cddbf682184b7e206
Yaroslav <proskurin.yarik@gmail.com>
2025-06-18 14:13:03 +0200

    feat(main.go): initialize basic HTTP server with root handler

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
-	
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my ToDo app")
+	})
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
